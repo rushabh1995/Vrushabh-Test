d2ede92257e9fef3f7b7b7bd10fd38db02bea487
system <system@cubecloud.dev>
2023-09-27 20:58:07 +0000

    Working dbt integration

--- model/defaults.py
+++ model/defaults.py
@@ -1,1 +1,18 @@
+from cube import template
 from cube_dbt import Dbt
+
+context = template.JinjaContext()
+
+dbt = (
+  Dbt
+  .from_file('model/manifest.json')
+  .filter(paths=['dimensions', 'fact'])
+)
+
+@context.function
+def dbt_models():
+  return dbt.models
+
+@context.function
+def dbt_model(name):
+  return dbt.model(name)
